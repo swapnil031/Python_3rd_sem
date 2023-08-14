8b2c23c5fb5aeab108d7b27f31247973e27b8773
swap031 <swapnildas953@gmail.com>
2023-08-15 00:22:34 +0530

    first commit

---
+++ playing_with_numbers.py
@@ -0,0 +1,12 @@
+from math import*
+print(-2)
+print(2+2.3*9)
+print(10%2)
+num=2
+print(num)
+print(str(num)+"is my favourie no")
+print(pow(num,2))
+print(max(num,3))
+print(round(3.57))
+print(floor(3.57))
+print(ceil(2.5))
---
+++ strings.py
@@ -0,0 +1,10 @@
+#charecter_name=input('enter your name:')
+charecter_age=15
+ismale=True
+#print('your name is '+ charecter_name + '\n and i love football ',charecter_age)
+#print(charecter_name.upper().isupper())
+#print(len(charecter_name))
+#print(charecter_name[0])
+phrase="hello world"
+print(phrase.index("h"))
+print(phrase.replace("world","arko"))
