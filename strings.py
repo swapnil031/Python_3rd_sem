eeb4f4b99fc0d6083b23f69b1759812ea9af99b5
swapnil <swapnildas953@gmail.com>
2023-08-26 23:09:37 +0530

    change

--- strings.py
+++ strings.py
@@ -1,7 +1,7 @@
-#charecter_name=input('enter your name:')
+charecter_name=input('enter your name:')
 charecter_age=15
 ismale=True
-#print('your name is '+ charecter_name + '\n and i love football ',charecter_age)
+print('your name is '+ charecter_name + '\n and i love football ',charecter_age)
 #print(charecter_name.upper().isupper())
 #print(len(charecter_name))
 #print(charecter_name[0])
