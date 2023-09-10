3bd718b0f97db55bbb33bdbdc87559fc339163c2
swapnil <swapnildas953@gmail.com>
2023-09-10 11:00:25 +0530

    2nd

--- list.py
+++ list.py
@@ -1,5 +1,7 @@
 friends=["soumik","srijan","uddalak","soutik","samiran"]
 lucky_numbers=[1,2,3,4,5]
+friends2=friends.copy() #copying list
+print(friends2)
 friends.append("hello")
 print(friends)
 friends.insert(1,"soudeep")
@@ -12,3 +14,8 @@ print(friends)
 friends.pop()
 print(friends)
 print(friends.index("soumik"))
+if 2 in lucky_numbers:
+    print("yes")
+else:
+    print("no")
+print(friends[1:7:2])
--- search.py
+++ search.py
@@ -1,16 +1,15 @@
-def search(a,list2):
+def search(a:int,list2):
     #print(n)
     for i in list2:
         #print(i)
-        print(type(i))
+        #print(type(i))
         #print(list2)
         if(i==a):
             return "found"
-    
     return "not found"
 #n=int(input("enter the size of the list"))
 num=[]
-num= [int(item) for item in input("enter number").split(',')]
+num= [int(item) for item in input("enter number").split(',')] #taking input of list in python
 print(num)
 print(len(num))
 print("enter the no you want to search:")
